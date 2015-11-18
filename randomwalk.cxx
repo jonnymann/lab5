46f93a4abe85d97dd025be2ad805009d20298fc7
jonnymann <jonny-stracke@web.de>
2015-11-18 17:59:44 +0100

    Update randomwalk.cxx

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -11,6 +12,36 @@ struct colloid{
 
 void init(colloid* const c, const int N);
 void print(const colloid* const c, const int N, const string fname);
+void condition(int* rx, int* ry, int N){
+  for(int k=0; k<N;k++){
+	    rx[k]=int(3*(double(rand())/RAND_MAX))-1;
+	    ry[k]=int(3*(double(rand())/RAND_MAX))-1;
+  }
+}
+
+void pusher(colloid* c, int* rx, int* ry, int N, const double step);
+
+void state(colloid* c, int N, double &meanx,double &meany,double &var){
+  meanx=0;
+  meany=0;
+  var=0;
+  
+  for (int j=0; j<N;j++){
+  meanx+= c[j].x;
+  }
+  meanx=meanx/N;
+  
+  for (int j=0; j<N;j++){
+  meany+= c[j].y;
+  }
+  meany=meany/N;
+  for (int j=0; j<N;j++){
+  var+=pow(c[j].x-meanx,2);
+  }
+  var=var/N;
+  
+}
+  
 
 int main(void){
   
@@ -44,9 +75,9 @@ int main(void){
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
-	    // call to function which randomly sets up rx and ry
-	    // call to function which pushes all colloids according to rx and ry
-	    // call to function which evaluates statistics
+	  condition(rx,ry,N);
+	  pusher(c,rx,ry,N,step);
+	  state(c,N,meanx,meany,var);
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
@@ -73,3 +104,12 @@ void print(const colloid* const c, const int N, const string fname){
 	out << c[i].x << "\t" << c[i].y << endl;
     out.close();
 }
+
+void pusher(colloid* c, int* rx, int* ry, int N, const double step)
+{
+    for(int k=0; k<N;k++){
+      c[k].x = c[k].x + (rx[k])*step;
+      c[k].y = c[k].y + (ry[k])*step;
+    }
+}
+
